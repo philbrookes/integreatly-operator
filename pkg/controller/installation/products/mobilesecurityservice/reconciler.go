44f1bc918473b9b68f42b8bde77adfaf4414afd3
Damien Murphy <damienomurchu@gmail.com>
2019-09-23 15:44:40 +0100

    add mobile security service install

---
+++ pkg/controller/installation/products/mobilesecurityservice/reconciler.go
@@ -0,0 +1,227 @@
+package mobilesecurityservice
+
+import (
+	"context"
+	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
+	mobilesecurityservice "github.com/integr8ly/integreatly-operator/pkg/apis/mobilesecurityservice/v1alpha1"
+	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/marketplace"
+	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/products/config"
+	"github.com/integr8ly/integreatly-operator/pkg/resources"
+	"github.com/operator-framework/operator-lifecycle-manager/pkg/lib/ownerutil"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
+	k8serr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	pkgclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	defaultInstallationNamespace = "mobile-security-service"
+	defaultSubscriptionName      = "integreatly-mobile-security-service"
+	clusterName                  = "integreatly-cluster"
+	serverClusterName            = "mobile-security-service"
+	dbClusterName                = "mobile-security-service-db"
+)
+
+type Reconciler struct {
+	Config        *config.MobileSecurityService
+	ConfigManager config.ConfigReadWriter
+	mpm           marketplace.MarketplaceInterface
+	logger        *logrus.Entry
+	*resources.Reconciler
+}
+
+func NewReconciler(configManager config.ConfigReadWriter, instance *v1alpha1.Installation, mpm marketplace.MarketplaceInterface) (*Reconciler, error) {
+	config, err := configManager.ReadMobileSecurityService()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not read mobile security service config")
+	}
+
+	if config.GetNamespace() == "" {
+		config.SetNamespace(instance.Spec.NamespacePrefix + defaultInstallationNamespace)
+	}
+
+	logger := logrus.NewEntry(logrus.StandardLogger())
+
+	return &Reconciler{
+		ConfigManager: configManager,
+		Config:        config,
+		mpm:           mpm,
+		logger:        logger,
+		Reconciler:    resources.NewReconciler(mpm),
+	}, nil
+}
+
+func (r *Reconciler) GetPreflightObject(ns string) runtime.Object {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mobile-security-service",
+			Namespace: ns,
+		},
+	}
+}
+
+// Reconcile reads that state of the cluster for amq streams and makes changes based on the state read
+// and what is required
+func (r *Reconciler) Reconcile(ctx context.Context, inst *v1alpha1.Installation, product *v1alpha1.InstallationProductStatus, serverClient pkgclient.Client) (v1alpha1.StatusPhase, error) {
+	ns := r.Config.GetNamespace()
+	if ns == "" {
+		// TODO return phase failed as default namespace should be set in NewReconciler
+	}
+	version, err := resources.NewVersion(v1alpha1.OperatorVersionMobileSecurityService)
+
+	phase, err := r.ReconcileNamespace(ctx, ns, inst, serverClient)
+	if err != nil || phase != v1alpha1.PhaseCompleted {
+		return phase, err
+	}
+
+	phase, err = r.ReconcileSubscription(ctx, inst, marketplace.Target{Namespace: ns, Channel: marketplace.IntegreatlyChannel, Pkg: defaultSubscriptionName}, serverClient, version)
+	if err != nil || phase != v1alpha1.PhaseCompleted {
+		return phase, err
+	}
+
+	phase, err = r.handleCreatingComponents(ctx, serverClient, inst)
+	if err != nil || phase != v1alpha1.PhaseCompleted {
+		return phase, err
+	}
+
+	phase, err = r.handleProgressPhase(ctx, serverClient)
+	if err != nil || phase != v1alpha1.PhaseCompleted {
+		return phase, err
+	}
+
+	product.Host = r.Config.GetHost()
+	product.Version = r.Config.GetProductVersion()
+
+	r.logger.Infof("%s has reconciled successfully", r.Config.GetProductName())
+	return v1alpha1.PhaseCompleted, nil
+}
+
+func (r *Reconciler) handleCreatingComponents(ctx context.Context, client pkgclient.Client, inst *v1alpha1.Installation) (v1alpha1.StatusPhase, error) {
+
+	r.logger.Debug("reconciling mobile security service db custom resource")
+
+	mssDb := &mobilesecurityservice.MobileSecurityServiceDB{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "MobileSecurityServiceDB",
+			APIVersion: fmt.Sprintf(
+				"%s/%s",
+				mobilesecurityservice.SchemeGroupVersion.Group,
+				mobilesecurityservice.SchemeGroupVersion.Version),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dbClusterName,
+			Namespace: r.Config.GetNamespace(),
+		},
+		Spec: mobilesecurityservice.MobileSecurityServiceDBSpec{
+			ContainerName:          "database",
+			DatabaseMemoryLimit:    "512Mi",
+			DatabaseMemoryRequest:  "512Mi",
+			DatabaseName:           "mobile_security_service",
+			DatabaseNameParam:      "POSTGRESQL_DATABASE",
+			DatabasePassword:       "postgres",
+			DatabasePasswordParam:  "POSTGRESQL_PASSWORD",
+			DatabasePort:           5432,
+			DatabaseStorageRequest: "1Gi",
+			DatabaseUser:           "postgresql",
+			DatabaseUserParam:      "POSTGRESQL_USER",
+			Image:                  "centos/postgresql-96-centos7",
+			Size:                   1,
+		},
+	}
+	ownerutil.EnsureOwner(mssDb, inst)
+
+	// attempt to create the mss db custom resource
+	if err := client.Create(ctx, mssDb); err != nil && !k8serr.IsAlreadyExists(err) {
+		return v1alpha1.PhaseFailed, errors.Wrap(err, "failed to get or create a mobile security service db custom resource")
+	}
+
+	r.logger.Debug("reconciling mobile security service custom resource")
+
+	mss := &mobilesecurityservice.MobileSecurityService{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "MobileSecurityService",
+			APIVersion: mobilesecurityservice.SchemeGroupVersion.String(),
+			//APIVersion: fmt.Sprintf(
+			//	"%s/%s",
+			//	&mobilesecurityservice.SchemeGroupVersion.Group,
+			//	&mobilesecurityservice.SchemeGroupVersion.Version),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serverClusterName,
+			Namespace: r.Config.GetNamespace(),
+		},
+		Spec: mobilesecurityservice.MobileSecurityServiceSpec{
+			AccessControlAllowCredentials: "false",
+			AccessControlAllowOrigin:      "*",
+			ClusterProtocol:               "https",
+			ConfigMapName:                 "mobile-security-service-config",
+			ContainerName:                 "application",
+			DatabaseHost:                  "mobile-security-service-db",
+			DatabaseName:                  "mobile_security_service",
+			DatabasePassword:              "postgres",
+			DatabaseUser:                  "postgresql",
+			Image:                         "quay.io/aerogear/mobile-security-service:0.2.2",
+			LogFormat:                     "json",
+			LogLevel:                      "info",
+			MemoryLimit:                   "128Mi",
+			MemoryRequest:                 "64Mi",
+			OAuthContainerName:            "oauth-proxy",
+			OAuthImage:                    "quay.io/openshift/origin-oauth-proxy:4.2.0",
+			OAuthMemoryLimit:              "64Mi",
+			OAuthMemoryRequest:            "32Mi",
+			OAuthResourceCpu:              "10m",
+			OAuthResourceCpuLimit:         "20m",
+			Port:                          3000,
+			ResourceCpu:                   "10m",
+			ResourceCpuLimit:              "20m",
+			RouteName:                     "route",
+			Size:                          1,
+		},
+	}
+	ownerutil.EnsureOwner(mss, inst)
+
+	// attempt to create the mss custom resource
+	if err := client.Create(ctx, mss); err != nil && !k8serr.IsAlreadyExists(err) {
+		return v1alpha1.PhaseFailed, errors.Wrap(err, "failed to get or create a mobile security service custom resource")
+	}
+
+	// if there are no errors, the phase is complete
+	return v1alpha1.PhaseCompleted, nil
+}
+
+func (r *Reconciler) handleProgressPhase(ctx context.Context, client pkgclient.Client) (v1alpha1.StatusPhase, error) {
+	r.logger.Debug("checking mobile security service pods are running")
+
+	pods := &v1.PodList{}
+	err := client.List(ctx, &pkgclient.ListOptions{Namespace: r.Config.GetNamespace()}, pods)
+	if err != nil {
+		return v1alpha1.PhaseFailed, errors.Wrap(err, "failed to check mobile security service installation")
+	}
+
+	//4 pods expected - operator, db, service
+	if len(pods.Items) < 3 { //TODO check if status value to check
+		return v1alpha1.PhaseInProgress, nil
+	}
+
+	//and they should all be ready
+checkPodStatus:
+	for _, pod := range pods.Items {
+		for _, cnd := range pod.Status.Conditions {
+			if cnd.Type == v1.ContainersReady {
+				if cnd.Status != v1.ConditionStatus("True") {
+					return v1alpha1.PhaseInProgress, nil
+				}
+				break checkPodStatus
+			}
+		}
+	}
+
+	r.logger.Infof("all pods ready, returning complete")
+	return v1alpha1.PhaseCompleted, nil
+}
---
+++ pkg/controller/installation/products/mobilesecurityservice/reconciler_test.go
@@ -0,0 +1,516 @@
+package mobilesecurityservice
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
+	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/marketplace"
+	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/products/config"
+	"github.com/operator-framework/operator-lifecycle-manager/pkg/lib/ownerutil"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	integreatlyv1alpha1 "github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
+	mss "github.com/integr8ly/integreatly-operator/pkg/apis/mobilesecurityservice/v1alpha1"
+	moqclient "github.com/integr8ly/integreatly-operator/pkg/client"
+	v1Route "github.com/openshift/api/route/v1"
+	coreosv1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1"
+	operatorsv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	marketplacev1 "github.com/operator-framework/operator-marketplace/pkg/apis/operators/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	pkgclient "sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func getBuildScheme() (*runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	err := integreatlyv1alpha1.SchemeBuilder.AddToScheme(scheme)
+	err = operatorsv1alpha1.AddToScheme(scheme)
+	err = marketplacev1.SchemeBuilder.AddToScheme(scheme)
+	err = corev1.SchemeBuilder.AddToScheme(scheme)
+	err = coreosv1.SchemeBuilder.AddToScheme(scheme)
+	err = mss.SchemeBuilder.AddToScheme(scheme)
+	err = v1Route.AddToScheme(scheme)
+	return scheme, err
+}
+
+func basicInstallation() *v1alpha1.Installation {
+	return &v1alpha1.Installation{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "installation",
+			Namespace: defaultInstallationNamespace,
+		},
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "installation",
+			APIVersion: v1alpha1.SchemeGroupVersion.String(),
+		},
+	}
+}
+
+func basicConfigMock() *config.ConfigReadWriterMock {
+	return &config.ConfigReadWriterMock{
+		ReadMobileSecurityServiceFunc: func() (ready *config.MobileSecurityService, e error) {
+			return config.NewMobileSecurityService(config.ProductConfig{}), nil
+		},
+		WriteConfigFunc: func(config config.ConfigReadable) error {
+			return nil
+		},
+	}
+}
+
+func TestReconciler_config(t *testing.T) {
+	cases := []struct {
+		Name           string
+		ExpectError    bool
+		ExpectedError  string
+		ExpectedStatus v1alpha1.StatusPhase
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     pkgclient.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *v1alpha1.Installation
+	}{
+		{
+			Name:           "test error on failed config",
+			ExpectError:    true,
+			ExpectedError:  "could not read mobile security service config: could not read mss config",
+			ExpectedStatus: v1alpha1.PhaseFailed,
+			Installation:   &v1alpha1.Installation{},
+			FakeClient:     fakeclient.NewFakeClient(),
+			FakeConfig: &config.ConfigReadWriterMock{
+				ReadMobileSecurityServiceFunc: func() (ready *config.MobileSecurityService, e error) {
+					return nil, errors.New("could not read mss config")
+				},
+			},
+		},
+		{
+			Name:         "test namespace is set without fail",
+			Installation: &v1alpha1.Installation{},
+			FakeClient:   fakeclient.NewFakeClient(),
+			FakeConfig: &config.ConfigReadWriterMock{
+				ReadMobileSecurityServiceFunc: func() (ready *config.MobileSecurityService, e error) {
+					return config.NewMobileSecurityService(config.ProductConfig{
+						"NAMESPACE": "",
+					}), nil
+				},
+			},
+		},
+		{
+			Name:           "test subscription phase with error from mpm",
+			ExpectedStatus: v1alpha1.PhaseFailed,
+			ExpectError:    true,
+			Installation:   &v1alpha1.Installation{},
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return errors.New("dummy error")
+				},
+			},
+			FakeClient: fakeclient.NewFakeClient(),
+			FakeConfig: basicConfigMock(),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			_, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil && err.Error() != tc.ExpectedError {
+				t.Fatalf("unexpected error : '%v', expected: '%v'", err, tc.ExpectedError)
+			}
+			if err == nil && tc.ExpectedError != "" {
+				t.Fatalf("expected error '%v' and got nil", tc.ExpectedError)
+			}
+		})
+	}
+}
+
+func TestReconciler_reconcileCustomResource(t *testing.T) {
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		Name           string
+		FakeClient     pkgclient.Client
+		FakeConfig     *config.ConfigReadWriterMock
+		Installation   *v1alpha1.Installation
+		ExpectError    bool
+		ExpectedStatus v1alpha1.StatusPhase
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+	}{
+		{
+			Name:           "Test reconcile custom resource returns in progress when successful created",
+			FakeClient:     fakeclient.NewFakeClientWithScheme(scheme),
+			FakeConfig:     basicConfigMock(),
+			Installation:   &v1alpha1.Installation{},
+			ExpectedStatus: v1alpha1.PhaseCompleted,
+		},
+		{
+			Name: "Test reconcile custom resource returns failed when cr status is failed",
+			FakeClient: &moqclient.SigsClientInterfaceMock{
+				CreateFunc: func(ctx context.Context, obj runtime.Object) error {
+					return errors.New("dummy create error")
+				},
+			},
+			FakeConfig:     basicConfigMock(),
+			Installation:   &v1alpha1.Installation{},
+			ExpectedStatus: v1alpha1.PhaseFailed,
+			ExpectError:    true,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			reconciler, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil {
+				t.Fatal("unexpected err ", err)
+			}
+
+			phase, err := reconciler.handleCreatingComponents(context.TODO(), tc.FakeClient, tc.Installation)
+			if tc.ExpectError && err == nil {
+				t.Fatal("expected an error but got none")
+			}
+			if !tc.ExpectError && err != nil {
+				t.Fatal("expected no error but got one ", err)
+			}
+			if tc.ExpectedStatus != phase {
+				t.Fatal("expected phase ", tc.ExpectedStatus, " but got ", phase)
+			}
+		})
+	}
+}
+
+func TestReconciler_handleProgress(t *testing.T) {
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	unreadyPods := []runtime.Object{}
+	for i := 0; i < 3; i++ {
+		unreadyPods = append(unreadyPods, &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("%s-%d", "mobile-security-service", i),
+				Namespace: defaultInstallationNamespace,
+			},
+			Status: corev1.PodStatus{
+				Conditions: []corev1.PodCondition{
+					corev1.PodCondition{
+						Type:   corev1.ContainersReady,
+						Status: corev1.ConditionUnknown,
+					},
+				},
+			},
+		})
+	}
+
+	readyPods := []runtime.Object{}
+	for i := 0; i < 3; i++ {
+		readyPods = append(readyPods, &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("%s-%d", "mobile-security-service", i),
+				Namespace: defaultInstallationNamespace,
+			},
+			Status: corev1.PodStatus{
+				Conditions: []corev1.PodCondition{
+					corev1.PodCondition{
+						Type:   corev1.ContainersReady,
+						Status: corev1.ConditionTrue,
+					},
+				},
+			},
+		})
+	}
+
+	cases := []struct {
+		Name           string
+		ExpectError    bool
+		ExpectedStatus v1alpha1.StatusPhase
+		ExpectedError  string
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     client.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *v1alpha1.Installation
+	}{
+		{
+			Name:           "test failure to list pods",
+			ExpectedStatus: v1alpha1.PhaseFailed,
+			ExpectedError:  "failed to check mss installation",
+			ExpectError:    true,
+			FakeClient: &moqclient.SigsClientInterfaceMock{
+				ListFunc: func(ctx context.Context, opts *client.ListOptions, list runtime.Object) error {
+					return errors.New("dummy create error")
+				},
+			},
+			FakeConfig:   basicConfigMock(),
+			Installation: &v1alpha1.Installation{},
+		},
+		{
+			Name:           "test unready pods returns phase in progress",
+			ExpectedStatus: v1alpha1.PhaseInProgress,
+			FakeClient:     moqclient.NewSigsClientMoqWithScheme(scheme, unreadyPods...),
+			FakeConfig:     basicConfigMock(),
+			Installation:   &v1alpha1.Installation{},
+		},
+		{
+			Name:           "test ready pods returns phase complete",
+			ExpectedStatus: v1alpha1.PhaseCompleted,
+			FakeClient:     moqclient.NewSigsClientMoqWithScheme(scheme, readyPods...),
+			FakeConfig:     basicConfigMock(),
+			Installation:   &v1alpha1.Installation{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			reconciler, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil && err.Error() != tc.ExpectedError {
+				t.Fatalf("unexpected error : '%v', expected: '%v'", err, tc.ExpectedError)
+			}
+
+			status, err := reconciler.handleProgressPhase(context.TODO(), tc.FakeClient)
+			if err != nil && !tc.ExpectError {
+				t.Fatalf("expected error but got one: %v", err)
+			}
+			if err == nil && tc.ExpectError {
+				t.Fatal("expected error but got none")
+			}
+			if status != tc.ExpectedStatus {
+				t.Fatalf("Expected status: '%v', got: '%v'", tc.ExpectedStatus, status)
+			}
+		})
+	}
+}
+
+func TestReconciler_fullReconcile(t *testing.T) {
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// initialise runtime objects
+	objs := []runtime.Object{}
+	objs = append(objs, &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: defaultInstallationNamespace,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					Name:       "installation",
+					APIVersion: v1alpha1.SchemeGroupVersion.String(),
+				},
+			},
+		},
+		Status: corev1.NamespaceStatus{
+			Phase: corev1.NamespaceActive,
+		},
+	}, &v1Route.Route{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Route",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: defaultInstallationNamespace,
+			Name:      "mobile-security-service",
+		},
+	})
+	for i := 0; i < 3; i++ {
+		objs = append(objs, &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("%s-%d", "mobile-security-service", i),
+				Namespace: defaultInstallationNamespace,
+			},
+			Status: corev1.PodStatus{
+				Conditions: []corev1.PodCondition{
+					corev1.PodCondition{
+						Type:   corev1.ContainersReady,
+						Status: corev1.ConditionTrue,
+					},
+				},
+			},
+		})
+	}
+
+	cases := []struct {
+		Name           string
+		ExpectError    bool
+		ExpectedStatus v1alpha1.StatusPhase
+		ExpectedError  string
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     client.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *v1alpha1.Installation
+		Product        *v1alpha1.InstallationProductStatus
+	}{
+		{
+			Name:           "test successful reconcile",
+			ExpectedStatus: v1alpha1.PhaseCompleted,
+			FakeClient:     moqclient.NewSigsClientMoqWithScheme(scheme, objs...),
+			FakeConfig:     basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient client.Client, subName string, ns string) (plan *operatorsv1alpha1.InstallPlanList, subscription *operatorsv1alpha1.Subscription, e error) {
+					return &operatorsv1alpha1.InstallPlanList{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "MobileSecurityService",
+								APIVersion: mss.SchemeGroupVersion.String(),
+							},
+							ListMeta: metav1.ListMeta{},
+							//ObjectMeta: metav1.ObjectMeta{
+							//	Name: "mss-install-plan",
+							//},
+							//Status: operatorsv1alpha1.InstallPlanStatus{
+							//	Phase: operatorsv1alpha1.InstallPlanPhaseComplete,
+							//},
+						}, &operatorsv1alpha1.Subscription{
+							Status: operatorsv1alpha1.SubscriptionStatus{
+								Install: &operatorsv1alpha1.InstallPlanReference{
+									Name: "mss-install-plan",
+								},
+							},
+						}, nil
+				},
+			},
+			Installation: basicInstallation(),
+			Product:      &v1alpha1.InstallationProductStatus{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			reconciler, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil && err.Error() != tc.ExpectedError {
+				t.Fatalf("unexpected error : '%v', expected: '%v'", err, tc.ExpectedError)
+			}
+
+			status, err := reconciler.Reconcile(context.TODO(), tc.Installation, tc.Product, tc.FakeClient)
+			if err != nil && !tc.ExpectError {
+				t.Fatalf("expected error but got one: %v", err)
+			}
+			if err == nil && tc.ExpectError {
+				t.Fatal("expected error but got none")
+			}
+			if status != tc.ExpectedStatus {
+				t.Fatalf("Expected status: '%v', got: '%v'", tc.ExpectedStatus, status)
+			}
+		})
+	}
+}
+
+func TestReconciler_testPhases(t *testing.T) {
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		Name           string
+		ExpectedStatus v1alpha1.StatusPhase
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     pkgclient.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *v1alpha1.Installation
+		Product        *v1alpha1.InstallationProductStatus
+	}{
+		{
+			Name:           "test namespace terminating returns phase in progress",
+			ExpectedStatus: v1alpha1.PhaseInProgress,
+			Installation:   basicInstallation(),
+			FakeClient: moqclient.NewSigsClientMoqWithScheme(scheme, &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: defaultInstallationNamespace,
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Name:       "installation",
+							APIVersion: v1alpha1.SchemeGroupVersion.String(),
+						},
+					},
+				},
+				Status: corev1.NamespaceStatus{
+					Phase: corev1.NamespaceTerminating,
+				},
+			}),
+			FakeConfig: basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient client.Client, subName string, ns string) (plan *operatorsv1alpha1.InstallPlanList, subscription *operatorsv1alpha1.Subscription, e error) {
+					return &operatorsv1alpha1.InstallPlanList{}, &operatorsv1alpha1.Subscription{}, nil
+				},
+			},
+			Product: &v1alpha1.InstallationProductStatus{},
+		},
+		{
+			Name:           "test subscription creating returns phase in progress",
+			ExpectedStatus: v1alpha1.PhaseInProgress,
+			Installation:   basicInstallation(),
+			FakeClient:     fakeclient.NewFakeClientWithScheme(scheme),
+			FakeConfig:     basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient client.Client, subName string, ns string) (*operatorsv1alpha1.InstallPlanList, *operatorsv1alpha1.Subscription, error) {
+					return &operatorsv1alpha1.InstallPlanList{}, &operatorsv1alpha1.Subscription{}, nil
+				},
+			},
+			Product: &v1alpha1.InstallationProductStatus{},
+		},
+		{
+			Name:           "test components creating returns phase in progress",
+			ExpectedStatus: v1alpha1.PhaseInProgress,
+			Installation:   basicInstallation(),
+			FakeClient:     fakeclient.NewFakeClientWithScheme(scheme),
+			FakeConfig:     basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient client.Client, sub string, ns string) (*operatorsv1alpha1.InstallPlanList, *operatorsv1alpha1.Subscription, error) {
+					return &operatorsv1alpha1.InstallPlanList{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "MobileSecurityService",
+								APIVersion: mss.SchemeGroupVersion.String(),
+							},
+							ListMeta: metav1.ListMeta{},
+							//ObjectMeta: metav1.ObjectMeta{
+							//	Name:      "mss-install-plan",
+							//	Namespace: "mobile-security-service",
+							//},
+							//Status: operatorsv1alpha1.InstallPlanStatus{
+							//	Phase: operatorsv1alpha1.InstallPlanPhaseComplete,
+							//},
+						}, &operatorsv1alpha1.Subscription{
+							Status: operatorsv1alpha1.SubscriptionStatus{
+								Install: &operatorsv1alpha1.InstallPlanReference{
+									Name: "mss-install-plan",
+								},
+							},
+						}, nil
+				},
+			},
+			Product: &v1alpha1.InstallationProductStatus{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			reconciler, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil {
+				t.Fatalf("unexpected error : '%v'", err)
+			}
+
+			status, err := reconciler.Reconcile(context.TODO(), tc.Installation, tc.Product, tc.FakeClient)
+			if err != nil {
+				t.Fatalf("expected no error but got one: %v", err)
+			}
+			if status != tc.ExpectedStatus {
+				t.Fatalf("Expected status: '%v', got: '%v'", tc.ExpectedStatus, status)
+			}
+		})
+	}
+}
