6ac313422c2832249a4adc4e3bd0ce6bf984b741
Phil Brookes <phil.brookes@gmail.com>
2020-12-16 10:23:11 +0000

    partial refactor

--- pkg/resources/poddistribution/poddistribution.go
+++ pkg/resources/poddistribution/poddistribution.go
@@ -9,13 +9,14 @@ import (
 	"github.com/sirupsen/logrus"
 	k8appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	k8sTypes "k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -25,14 +26,11 @@ const (
 	// Annotation counter on the pods controller, dc, rs, ss
 	PodRebalanceAttempts = "pod-rebalance-attempts"
 	maxBalanceAttempts   = 3
-	name                 = "name"
-	ns                   = "ns"
-	kind                 = "kind"
 )
 
 type KindNameSpaceName struct {
 	*k8sTypes.NamespacedName
-	Kind string
+	Kind runtime.Object
 }
 
 func (knn KindNameSpaceName) String() string {
@@ -40,8 +38,12 @@ func (knn KindNameSpaceName) String() string {
 }
 
 // Check the PodBalanceAttempts, ensure less than maxBalanceAttempts
-func verifyRebalanceCount(ctx context.Context, client k8sclient.Client, obj metav1.Object) (bool, error) {
-	ant := obj.GetAnnotations()
+func verifyRebalanceCount(ctx context.Context, client k8sclient.Client, obj runtime.Object) (bool, error) {
+	metaObj, err := meta.Accessor(obj)
+	if err != nil {
+		return false, nil
+	}
+	ant := metaObj.GetAnnotations()
 	// If there are no annotations then 0 balance attempts have been made
 	if ant == nil {
 		return true, nil
@@ -52,7 +54,7 @@ func verifyRebalanceCount(ctx context.Context, client k8sclient.Client, obj meta
 			return false, fmt.Errorf("Error converting string annotations %s", ant[PodRebalanceAttempts])
 		} else {
 			if i >= maxBalanceAttempts {
-				logrus.Warningf("Reached max balance attempts for %s on %s", obj.GetName(), obj.GetNamespace())
+				logrus.Warningf("Reached max balance attempts for %s on %s", metaObj.GetName(), metaObj.GetNamespace())
 				return false, nil
 			}
 		}
@@ -61,57 +63,14 @@ func verifyRebalanceCount(ctx context.Context, client k8sclient.Client, obj meta
 	return true, nil
 }
 
-func getObject(ctx context.Context, client k8sclient.Client, objDtls map[string]string) (metav1.Object, error) {
-	if objDtls[kind] == "DeploymentConfig" {
-		return getDeploymentConfig(ctx, client, objDtls)
-	}
-	if objDtls[kind] == "ReplicaSet" {
-		return getReplicaSet(ctx, client, objDtls)
-	}
-	if objDtls[kind] == "StatefulSet" {
-		return getStatefulSet(ctx, client, objDtls)
-	}
-	return nil, fmt.Errorf("Unexpected obj kind %s", objDtls[kind])
-}
-
-func getDeploymentConfig(ctx context.Context, client k8sclient.Client, objDtls map[string]string) (metav1.Object, error) {
-	deploymentConfig := &appsv1.DeploymentConfig{}
-	err := client.Get(ctx, k8sTypes.NamespacedName{Name: objDtls[name], Namespace: objDtls[ns]}, deploymentConfig)
+func getObject(ctx context.Context, client k8sclient.Client, knn *KindNameSpaceName) (runtime.Object, error) {
+	object := knn.Kind
+	err := client.Get(ctx, k8sTypes.NamespacedName{Name: knn.Name, Namespace: knn.Namespace}, object)
 	if err != nil {
 		return nil, fmt.Errorf("Error getting DeploymentConfig: %w", err)
 	}
-	return deploymentConfig, nil
-}
+	return object, nil
 
-func getReplicaSet(ctx context.Context, client k8sclient.Client, objDtls map[string]string) (metav1.Object, error) {
-	rs := &k8appsv1.ReplicaSet{}
-	err := client.Get(ctx, k8sTypes.NamespacedName{Name: objDtls[name], Namespace: objDtls[ns]}, rs)
-	if err != nil {
-		return nil, fmt.Errorf("Error getting ReplicaSet: %w", err)
-	}
-	return rs, nil
-}
-
-func getStatefulSet(ctx context.Context, client k8sclient.Client, objDtls map[string]string) (metav1.Object, error) {
-	ss := &k8appsv1.StatefulSet{}
-	err := client.Get(ctx, k8sTypes.NamespacedName{Name: objDtls[name], Namespace: objDtls[ns]}, ss)
-	if err != nil {
-		logrus.Errorf("Error getting StatefulSet %v", err)
-		return nil, err
-	}
-	return ss, nil
-}
-
-func parseKnn(knn string) (map[string]string, error) {
-	objDtls := map[string]string{}
-	s := strings.Split(knn, "/")
-	if len(s) != 3 {
-		return nil, fmt.Errorf("Error parsing knn :%s", knn)
-	}
-	objDtls[kind] = s[0]
-	objDtls[ns] = s[1]
-	objDtls[name] = s[2]
-	return objDtls, nil
 }
 
 func getNamespaces(nsPrefix string, installType string) []string {
@@ -147,12 +106,7 @@ func ReconcilePodDistribution(ctx context.Context, client k8sclient.Client, nsPr
 			continue
 		}
 		for knn, pods := range unbalanced {
-			objDtls, err := parseKnn(knn)
-			if err != nil {
-				mErr.Add(err)
-				continue
-			}
-			obj, err := getObject(ctx, client, objDtls)
+			obj, err := getObject(ctx, client, knn)
 			if err != nil {
 				mErr.Add(err)
 				continue
@@ -163,7 +117,7 @@ func ReconcilePodDistribution(ctx context.Context, client k8sclient.Client, nsPr
 				continue
 			}
 			if rebalance {
-				err := forceRebalance(ctx, client, objDtls, pods)
+				err := forceRebalance(ctx, client, knn, pods)
 				if err != nil {
 					mErr.Add(err)
 				}
@@ -173,9 +127,9 @@ func ReconcilePodDistribution(ctx context.Context, client k8sclient.Client, nsPr
 	return mErr
 }
 
-func findUnbalanced(ctx context.Context, nameSpace string, client k8sclient.Client) (map[string][]string, error) {
+func findUnbalanced(ctx context.Context, nameSpace string, client k8sclient.Client) (map[*KindNameSpaceName][]string, error) {
 	nodes := &corev1.NodeList{}
-	unBalanced := map[string][]string{}
+	unBalanced := map[*KindNameSpaceName][]string{}
 	if err := client.List(ctx,
 		nodes, &k8sclient.ListOptions{}); err != nil {
 		return unBalanced, err
@@ -190,8 +144,8 @@ func findUnbalanced(ctx context.Context, nameSpace string, client k8sclient.Clie
 		}
 	}
 	logrus.Debugf("nodes to zone %v", nodesToZone)
-	balance := map[string][]string{}
-	objPods := map[string][]string{}
+	balance := map[*KindNameSpaceName][]string{}
+	objPods := map[*KindNameSpaceName][]string{}
 	l := &corev1.PodList{}
 	listOpts := []k8sclient.ListOption{
 		k8sclient.InNamespace(nameSpace),
@@ -201,7 +155,7 @@ func findUnbalanced(ctx context.Context, nameSpace string, client k8sclient.Clie
 	}
 
 	// need to check if there is more than 1 pod of a kind
-	podCount := map[string]int{}
+	podCount := map[*KindNameSpaceName]int{}
 	logrus.Debugf("total pods in ns %s: %d", nameSpace, len(l.Items))
 	for _, p := range l.Items {
 		if p.Status.Phase != "Running" {
@@ -218,28 +172,31 @@ func findUnbalanced(ctx context.Context, nameSpace string, client k8sclient.Clie
 
 				if o.Kind == "ReplicationController" {
 					knn.Name = p.Annotations["openshift.io/deployment-config.name"]
-					knn.Kind = "DeploymentConfig"
-				} else {
+					knn.Kind = &appsv1.DeploymentConfig{}
+				} else if o.Kind == "StatefulSet" {
 					knn.Name = o.Name
-					knn.Kind = o.Kind
+					knn.Kind = &k8appsv1.StatefulSet{}
+				} else if o.Kind == "Deployment" {
+					knn.Name = o.Name
+					knn.Kind = &k8appsv1.Deployment{}
 				}
-				if _, ok := podCount[knn.String()]; !ok {
-					podCount[knn.String()] = 1
+				if _, ok := podCount[knn]; !ok {
+					podCount[knn] = 1
 				} else {
-					podCount[knn.String()] = podCount[knn.String()] + 1
+					podCount[knn] = podCount[knn] + 1
 				}
-				if balance[knn.String()] == nil {
-					balance[knn.String()] = []string{}
+				if balance[knn] == nil {
+					balance[knn] = []string{}
 				}
 
-				if objPods[knn.String()] == nil {
-					objPods[knn.String()] = []string{}
+				if objPods[knn] == nil {
+					objPods[knn] = []string{}
 				}
-				objPods[knn.String()] = append(objPods[knn.String()], p.Name)
+				objPods[knn] = append(objPods[knn], p.Name)
 
 				zone := nodesToZone[p.Status.HostIP]
-				if !zoneExists(balance[knn.String()], zone) {
-					balance[knn.String()] = append(balance[knn.String()], nodesToZone[p.Status.HostIP])
+				if !zoneExists(balance[knn], zone) {
+					balance[knn] = append(balance[knn], nodesToZone[p.Status.HostIP])
 				}
 				break
 			}
@@ -265,12 +222,12 @@ func zoneExists(zones []string, zone string) bool {
 }
 
 // Delete a single pod to force redistribution
-func forceRebalance(ctx context.Context, client k8sclient.Client, objDtls map[string]string, pods []string) error {
+func forceRebalance(ctx context.Context, client k8sclient.Client, knn KindNameSpaceName, pods []string) error {
 
-	deletePod(ctx, client, pods[0], objDtls[ns])
+	deletePod(ctx, client, pods[0], knn.Namespace)
 	// The wait prevents version clash errors when updating the controller
 	err := wait.Poll(time.Second*5, time.Second*5, func() (done bool, err error) {
-		if objDtls[kind] == "DeploymentConfig" {
+		if knn.Kind.GetObjectKind().GroupVersionKind().Kind == "DeploymentConfig" {
 			err = updatePodBalanceAttemptsOnDeploymentConfig(objDtls, ctx, client)
 		} else if objDtls[kind] == "ReplicaSet" {
 			err = updatePodBalanceAttemptsOnReplicaSet(objDtls, ctx, client)
@@ -308,98 +265,30 @@ func deletePod(ctx context.Context, client k8sclient.Client, podName string, ns 
 	}
 }
 
-func updatePodBalanceAttemptsOnDeploymentConfig(objDtls map[string]string, ctx context.Context, client k8sclient.Client) error {
-	dcName := objDtls[name]
-	ns := objDtls[ns]
-	logrus.Infof("Updating pod balance annotation %s, on ns %s", dcName, ns)
+func updatePodBalanceAttemptsOnKNN(ctx context.Context, knn KindNameSpaceName, client k8sclient.Client) error {
+	obj := knn.Kind
 
-	dc := &appsv1.DeploymentConfig{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      dcName,
-			Namespace: ns,
-		},
-	}
 	err := client.Get(ctx, k8sclient.ObjectKey{
-		Name:      dcName,
-		Namespace: ns,
-	}, dc)
+		Name:      knn.Name,
+		Namespace: knn.Namespace,
+	}, obj)
+
 	if err != nil {
-		return fmt.Errorf("Error getting dc %s on namespace %s. %w", ns, ns, err)
+		return fmt.Errorf("Error getting dc %s on namespace %s. %w", knn.Name, knn.Namespace, err)
 	}
-	ant, err := getAnnotations(dc.GetAnnotations(), dcName, ns)
+	metaObj, err := meta.Accessor(obj)
+	if err != nil {
+		return err
+	}
+	ant, err := getAnnotations(metaObj.GetAnnotations(), knn.Name, ns)
 	if err != nil {
 		return err
 	}
 	dc.SetAnnotations(ant)
-	if err := client.Update(ctx, dc); err != nil {
-		return fmt.Errorf("Error Updating dc %s on %s. %w", dcName, ns, err)
+	if err := client.Update(ctx, obj); err != nil {
+		return fmt.Errorf("Error Updating dc %s on %s. %w", knn.Name, knn.Namespace, err)
 	}
-	logrus.Infof("Successfully updated dc %s on %s", dcName, ns)
-	return nil
-}
-
-func updatePodBalanceAttemptsOnReplicaSet(objDtls map[string]string, ctx context.Context, client k8sclient.Client) error {
-	name := objDtls[name]
-	ns := objDtls[ns]
-	logrus.Infof("Updating pod balance annotation on replicaset %s, on ns %s", name, ns)
-
-	rs := &k8appsv1.ReplicaSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: ns,
-		},
-	}
-	err := client.Get(ctx, k8sclient.ObjectKey{
-		Name:      name,
-		Namespace: ns,
-	}, rs)
-	if err != nil {
-		return fmt.Errorf("Error getting ReplicaSet %s on namespace %s. %w", ns, ns, err)
-	}
-
-	ant, err := getAnnotations(rs.GetAnnotations(), rs.GetName(), rs.GetNamespace())
-	if err != nil {
-		return err
-	}
-	rs.SetAnnotations(ant)
-
-	if err := client.Update(ctx, rs); err != nil {
-		return fmt.Errorf("Error Updating ReplicaSet %s on %s. %w", rs.Name, rs.Namespace, err)
-	}
-	logrus.Infof("Successfully updated ReplicaSet %s on %s", name, ns)
-	return nil
-}
-
-func updatePodBalanceAttemptsOnStatefulSet(objDtls map[string]string, ctx context.Context, client k8sclient.Client) error {
-	name := objDtls[name]
-	ns := objDtls[ns]
-	logrus.Infof("Updating pod balance ss annotation %s, on ns %s", name, ns)
-
-	ss := &k8appsv1.StatefulSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: ns,
-		},
-	}
-	err := client.Get(ctx, k8sclient.ObjectKey{
-		Name:      name,
-		Namespace: ns,
-	}, ss)
-	if err != nil {
-		return fmt.Errorf("Error getting ss %s on namespace %s. %w", ns, ns, err)
-	}
-
-	if _, err := controllerutil.CreateOrUpdate(ctx, client, ss, func() error {
-		ant, err := getAnnotations(ss.GetAnnotations(), ss.GetName(), ss.GetNamespace())
-		if err != nil {
-			return err
-		}
-		ss.SetAnnotations(ant)
-		return nil
-	}); err != nil {
-		return fmt.Errorf("Error Updating ss %s on %s. %w", ss.Name, ss.Namespace, err)
-	}
-	logrus.Infof("Successfully updated statefulset %s on %s", name, ns)
+	logrus.Infof("Successfully updated dc %s on %s", knn.Name, knn.Namespace)
 	return nil
 }
 
